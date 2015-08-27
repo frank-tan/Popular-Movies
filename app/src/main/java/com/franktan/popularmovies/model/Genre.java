811b78fcac9d7295dd463b3bead7d285e0dd5aba
Frank Tan <francis.tine@gmail.com>
2015-08-27 23:25:14 +0800

    Added new model classes; refactored Movie Class to use Retrofit and Gson

---
+++ app/src/main/java/com/franktan/popularmovies/model/Genre.java
@@ -0,0 +1,52 @@
+package com.franktan.popularmovies.model;
+
+/**
+ * Created by tan on 27/08/2015.
+ */
+public class Genre {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Genre genre = (Genre) o;
+
+        if (id != genre.id) return false;
+        return name.equals(genre.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/franktan/popularmovies/model/Trailer.java
@@ -0,0 +1,7 @@
+package com.franktan.popularmovies.model;
+
+/**
+ * Created by tan on 27/08/2015.
+ */
+public class Trailer {
+}
