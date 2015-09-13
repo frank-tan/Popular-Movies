aa745c78c7b2206b7c22b9c7d469f9eda0733ee9
Frank Tan <francis.tine@gmail.com>
2015-09-13 22:37:29 +0800

    Renamed MainActivity to MovieGridActivity as a new activity is designed to be used as a launcher activity.

--- app/src/main/java/com/franktan/popularmovies/ui/activities/MovieGridActivity.java
+++ app/src/main/java/com/franktan/popularmovies/ui/activities/MovieGridActivity.java
@@ -11,7 +11,7 @@ import com.franktan.popularmovies.sync.MovieSyncAdapter;
 import com.franktan.popularmovies.ui.fragments.MovieDetailFragment;
 import com.franktan.popularmovies.ui.fragments.MoviesGridFragment;
 
-public class MainActivity extends AppCompatActivity
+public class MovieGridActivity extends AppCompatActivity
         implements MoviesGridFragment.OnFragmentInteractionListener {
 
     private boolean mTwoPaneMode;
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movie_grid);
 
         MovieSyncAdapter.initialize(this);
 
