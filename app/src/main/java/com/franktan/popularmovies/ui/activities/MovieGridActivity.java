aba490d749abbe8117553d728bb7d92810dbfd17
Frank Tan <francis.tine@gmail.com>
2015-09-17 23:04:34 +0800

    Removed setting activity as tabs are provided to choose between different filtering

--- app/src/main/java/com/franktan/popularmovies/ui/activities/MovieGridActivity.java
+++ app/src/main/java/com/franktan/popularmovies/ui/activities/MovieGridActivity.java
@@ -1,14 +1,11 @@
 package com.franktan.popularmovies.ui.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.BuildConfig;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.franktan.popularmovies.R;
 import com.franktan.popularmovies.model.MovieGroup;
@@ -25,8 +22,6 @@ public class MovieGridActivity extends AppCompatActivity
     private ViewPager mMovieGroupViewPager;
     private MovieGroupViewPagerAdapter mMovieGroupViewAdapter;
     private TabLayout mTabLayout;
-    private int mNumOfTabs = 3;
-    private CharSequence mTabTitles[] = {"Popular", "Top Rated", "Favourite"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +31,11 @@ public class MovieGridActivity extends AppCompatActivity
 
         MovieSyncAdapter.initialize(this);
 
-//        if(findViewById(R.id.fragment_movie_details) != null) {
-//            mTwoPaneMode = true;
-//        } else {
-//            mTwoPaneMode = false;
-//        }
+        if(findViewById(R.id.fragment_movie_details) != null) {
+            mTwoPaneMode = true;
+        } else {
+            mTwoPaneMode = false;
+        }
 
         if (BuildConfig.DEBUG) {
             Picasso.with(getApplicationContext()).setIndicatorsEnabled(true);
@@ -57,30 +52,6 @@ public class MovieGridActivity extends AppCompatActivity
 
         mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
         mTabLayout.setupWithViewPager(mMovieGroupViewPager);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent i = new Intent(this, SettingsActivity.class);
-            startActivity(i);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
